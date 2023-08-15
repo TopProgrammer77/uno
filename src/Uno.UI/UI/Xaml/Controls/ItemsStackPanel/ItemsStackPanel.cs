7f209a134732c29a0dbdc3b5ab548ae633250c8b
TopProgrammer77 <TopProgrammer77@gmail.com>
2023-08-15 13:58:26 -0400

    fix(TreeView): allow dragging and dropping of treeview items

--- src/Uno.UI/UI/Xaml/Controls/ItemsStackPanel/ItemsStackPanel.cs
+++ src/Uno.UI/UI/Xaml/Controls/ItemsStackPanel/ItemsStackPanel.cs
@@ -2,12 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using Uno;
+using Uno.Extensions;
+using Uno.Extensions.Specialized;
 using Uno.UI;
+using Windows.UI.Xaml.Media;
 
 namespace Windows.UI.Xaml.Controls
 {
-	public partial class ItemsStackPanel : Panel, IVirtualizingPanel
+	public partial class ItemsStackPanel : Panel, IVirtualizingPanel, IInsertionPanel
 	{
 		VirtualizingPanelLayout _layout;
 
@@ -56,6 +60,43 @@ namespace Windows.UI.Xaml.Controls
 #if !XAMARIN_IOS
 				_layout.BindToEquivalentProperty(this, nameof(CacheLength));
 #endif
+			}
+		}
+
+		void IInsertionPanel.GetInsertionIndexes(Windows.Foundation.Point position, out int first, out int second)
+		{
+			first = -1;
+			second = -1;
+			if ((new Windows.Foundation.Rect(0, 0, ActualSize.X, ActualSize.Y)).Contains(position))
+			{
+				if (_children == null || _children.Empty())
+				{
+					return;
+				}
+				if (Orientation == Orientation.Vertical)
+				{
+					foreach (var child in _children)
+					{
+						if (position.Y >= child.ActualOffset.Y + child.ActualSize.Y / 2)
+						{
+							first++;
+						}
+					}
+				}
+				else
+				{
+					foreach (var child in _children)
+					{
+						if (position.X >= child.ActualOffset.X + child.ActualSize.X / 2)
+						{
+							first++;
+						}
+					}
+				}
+				if (first + 1 < _children.Count)
+				{
+					second = first + 1;
+				}
 			}
 		}
 	}
